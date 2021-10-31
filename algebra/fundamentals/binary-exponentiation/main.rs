b6f39cd57fce0bcac41cd0738376267b043c18e2
Ionut Cicio <ionutcicio.dev@gmail.com>
2021-10-31 15:04:49 +0100

    binary exponentiation

--- algebra/fundamentals/binary-exponentiation/main.rs
+++ algebra/fundamentals/binary-exponentiation/main.rs
@@ -1,24 +1,47 @@
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		assert_eq!(2 + 2, 4);
-	}
-}
-
 fn recursive_binary_pow(base: u64, exponent: u64) -> u64 {
 	if exponent == 0 {
 		return 1;
 	}
 
 	let result = recursive_binary_pow(base, exponent >> 1);
-	if exponent & 1 {
+	if exponent & 1 == 1{
 		return result * result * base;
 	} else {
 		return result * result
 	}
 }
 
+fn binary_pow(mut base: u64, mut exponent: u64) -> u64 {
+	let mut result = 1;
+
+	while exponent > 0 {
+		if exponent & 1 == 1 {
+			result *= base;
+		}
+		base *= base;
+		exponent >>= 1;
+	}
+
+	result
+}
+
+fn modular_binary_pow(mut base: u64, mut exponent: u64, modulo: u64) -> u64 {
+	let mut result = 1;
+	base %= modulo;
+
+	while exponent > 0 { 
+		if exponent & 1 == 1 {
+			result *= base % modulo;
+		}
+		base *= base % modulo;
+		exponent >>= 1;
+	}
+
+	result % modulo
+}
+
 fn main() {
-	println!("Hello")
+	assert_eq!(binary_pow(3, 13), 1594323);
+	assert_eq!(recursive_binary_pow(3, 13), 1594323);
+	assert_eq!(modular_binary_pow(3, 13, 100), 23);
 }
